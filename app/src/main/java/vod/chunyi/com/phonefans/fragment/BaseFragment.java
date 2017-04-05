4944a21b3804454570b482336dc3139e52418dc6
knight_hjj <1286264253@qq.com>
2017-04-05 10:25:27 +0800

    day one 4/5

---
+++ app/src/main/java/vod/chunyi/com/phonefans/fragment/BaseFragment.java
@@ -0,0 +1,13 @@
+package vod.chunyi.com.phonefans.fragment;
+
+import android.support.v4.app.Fragment;
+
+
+/**
+ * Created by knight on 2017/4/5.
+ */
+
+public class BaseFragment extends Fragment {
+
+
+}
